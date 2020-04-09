b3f76d2451d87adb19369eea90ad33db5b1b09da
slatermorgan <slatermorgan@hotmail.com>
2020-04-09 18:57:47 +0100

    Import auth

--- src/peaks/peaks.controller.ts
+++ src/peaks/peaks.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Query, ValidationPipe, Param, UsePipes, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, ValidationPipe, Param, UsePipes, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreatePeakDto } from './dto/create-peak.dto';
 import { PeaksService } from './peaks.service';
 import { Peak } from './peak.entity';
 import { GetPeaksFilterDto } from './dto/get-peaks-filter.dto';
 
 @Controller('peaks')
+@UseGuards(AuthGuard())
 export class PeaksController {
     constructor(private peaksService: PeaksService) {}
 
