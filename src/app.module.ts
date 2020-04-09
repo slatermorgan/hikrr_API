3c64d111f46da8aa6b34198b562c674f79acf0eb
slatermorgan <slatermorgan@hotmail.com>
2020-04-09 19:17:29 +0100

    Add auth module final part

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PeaksModule } from './peaks/peaks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [PeaksModule, TypeOrmModule.forRoot(typeOrmConfig)]
+    imports: [PeaksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule]
 })
 export class AppModule {}
--- src/peaks/peaks.module.ts
+++ src/peaks/peaks.module.ts
@@ -3,10 +3,12 @@ import { PeaksController } from './peaks.controller';
 import { PeaksService } from './peaks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PeakRepository } from './peaks.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([PeakRepository])
+        TypeOrmModule.forFeature([PeakRepository]),
+        AuthModule,
     ],
     controllers: [PeaksController],
     providers: [PeaksService]
